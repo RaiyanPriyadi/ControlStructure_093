abdf2bf4b581f40f7947bec5f561dde4be40876d
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-09-22 11:09:11 +0700

    menambahkan else tidak ada angka terbesar jika nilai angka sama

--- FindtheLargestofThreeNumbers.py
+++ FindtheLargestofThreeNumbers.py
@@ -2,11 +2,15 @@ angka1 = int(input("Masukkan angka pertama: "))
 angka2 = int(input("Masukkan angka kedua: "))
 angka3 = int(input("Masukkan angka ketiga: "))
 
-if (angka1 >= angka2) and (angka1 >= angka3):
+if (angka1 > angka2) and (angka1 > angka3):
     terbesar = angka1
-elif (angka2 >= angka1) and (angka2 >= angka3):
+    print("Angka terbesar adalah:", terbesar)
+elif (angka2 > angka1) and (angka2 > angka3):
     terbesar = angka2
-else:
+    print("Angka terbesar adalah:", terbesar)
+elif (angka3 > angka1) and (angka3 > angka2):
     terbesar = angka3
+    print("Angka terbesar adalah:", terbesar)
+else:
+    print("tidak ada angka terbesar.")    
 
-print("Angka terbesar adalah:", terbesar)
